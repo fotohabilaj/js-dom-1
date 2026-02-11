bc48984648280364b0a0a96033bafd02307dee5c
fotohabilaj <fotohabilaj@gmail.com>
2026-02-11 17:36:09 +0100

    add variables function eventlistener

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-console.log('asdf');
+console.log(document);
+const lamp = document.getElementById('lamp');
+const btn = document.getElementById('btn');
+let on_off = btn.innerHTML
+console.log(lamp);
 
-const lampadina = document.getElementById(lampadina);
-const bottone = document.querySelectorAll(bottone);
+function interrutore_luce(){
+    if(on_off === 'accendi'){
+        lamp.src = "img/yellow_lamp.png"
+        on_off  = 'spegni';
+    } else{
+        on_off = 'accendi';
+      lamp.src = "img/white_lamp.png"
+    }
+}
 
+btn.addEventListener('click', interrutore_luce)
