99e47f236de778957cfa9e1fb74dd2c4eea778e3
fotohabilaj <fotohabilaj@gmail.com>
2026-02-12 16:01:54 +0100

    corrected non switching text

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 console.log(document);
 const lamp = document.getElementById('lamp');
 const btn = document.getElementById('btn');
-let on_off = btn.innerHTML
-console.log(lamp);
 
 function interrutore_luce(){
-    if(on_off === 'Accendi'){
+    if(btn.innerHTML === 'Accendi'){
         lamp.src = "img/yellow_lamp.png"
-        on_off  = 'Spegni';
+        btn.innerHTML ='Spegni';
     } else{
-        on_off = 'Accendi';
+        btn.innerHTML ='Accendi';
       lamp.src = "img/white_lamp.png"
     }
 }
